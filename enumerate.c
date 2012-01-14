85be7dd17d844896ae2dfa0933d126fed81e2bfa
Frode Austvik <edorfaus@xepher.net>
2012-01-14 14:06:54 +0100

    Modified the enumerate program to use the new tempered library. This removes some of its capabilities, such as listing all USB devices and some of the information on them, and refocuses on just listing the devices that the library finds and can use, with the information it provides.

--- enumerate.c
+++ enumerate.c
@@ -1,82 +1,46 @@
-#include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 
-#include "hidapi.h"
-#include "temper_type.h"
+#include "tempered.h"
+
+void print_error( void )
+{
+	fprintf( stderr, "%s\n", tempered_error() );
+}
 
 int main( int argc, char **argv )
 {
-	struct hid_device_info *devs, *info;
-	bool list_all = false;
-	int i;
-	
-	for ( i = 1; i < argc; i++ )
+	if ( !tempered_init() )
 	{
-		if ( strcmp( "-a", argv[i] ) == 0 )
-		{
-			list_all = true;
-		}
-	}
-	
-	if ( hid_init() != 0 )
-	{
-		fprintf( stderr, "Could not initialize the HID API.\n" );
+		print_error();
 		return 1;
 	}
-	devs = hid_enumerate( 0, 0 );
-	if ( !devs )
+	
+	struct tempered_device_list *list = tempered_enumerate();
+	if ( list == NULL )
 	{
-		printf( "No HID devices were found.\n" );
+		print_error();
 	}
-	for ( info = devs; info; info = info->next )
+	else
 	{
-		temper_type* type = get_temper_type( info );
-		if ( list_all || ( type != NULL && !type->ignored ) )
+		struct tempered_device_list *dev;
+		for ( dev = list ; dev != NULL ; dev = dev->next )
 		{
 			printf(
-				"Device %04hx:%04hx if %d rel %4hx | %s | %ls %ls\n",
-				info->vendor_id, info->product_id,
-				info->interface_number, info->release_number,
-				info->path,
-				info->manufacturer_string, info->product_string
+				"Found device: %04x:%04x %i | %s | %s\n",
+				dev->type->vendor_id, dev->type->product_id,
+				dev->type->interface_number,
+				dev->path,
+				dev->type->name
 			);
-			if ( list_all )
-			{
-				/* Usage and usage page is only valid on Windows/Mac :(
-				if ( info->usage_page != 0 || info->usage != 0 )
-				{
-					printf(
-						"\tUsage page %hd, usage %hd\n",
-						info->usage_page, info->usage
-					);
-				}
-				// */
-				if ( info->serial_number != NULL )
-				{
-					printf( "\tSerial number: %ls\n", info->serial_number );
-				}
-			}
-			if ( list_all && type != NULL )
-			{
-				if ( type->ignored )
-				{
-					printf(
-						"\tRecognized as ignored part of: %s\n", type->name
-					);
-				}
-				else
-				{
-					printf( "\tRecognized as: %s\n", type->name );
-				}
-			}
 		}
+		
+		tempered_free_device_list( list );
 	}
-	hid_free_enumeration( devs );
-	if ( hid_exit() != 0 )
+	
+	if ( !tempered_exit() )
 	{
-		fprintf( stderr, "Error shutting down the HID API.\n" );
+		print_error();
+		return 1;
 	}
 	return 0;
 }
