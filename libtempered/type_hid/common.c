fba2c830520a559573bdc04b7432e5b6a3ec20e1
Frode Austvik <edorfaus@xepher.net>
2012-03-04 16:40:16 +0100

    Moved the HID device data structure out into an internal.h file.

--- libtempered/type_hid/common.c
+++ libtempered/type_hid/common.c
@@ -5,25 +5,10 @@
 #include <hidapi.h>
 
 #include "common.h"
+#include "internal.h"
 
 #include "../tempered.h"
 #include "../tempered-internal.h"
-
-/** The struct that is stored in device->data for this type of device. */
-struct temper_type_hid_device_data
-{
-	/** Handle for the HID device. */
-	hid_device *hid_dev;
-	
-	/** Length of data that has been read from the device. */
-	int data_length;
-	
-	/** The data that has been read from the device. */
-	unsigned char data[64];
-	
-	/** The HID device info for this device. */
-	struct temper_type_hid_data *dev_info;
-};
 
 /** Initialize the HID TEMPer types. */
 bool temper_type_hid_init( char **error )
---
+++ libtempered/type_hid/internal.h
@@ -0,0 +1,22 @@
+#ifndef TEMPER_TYPE_HID_INTERNAL_H
+#define TEMPER_TYPE_HID_INTERNAL_H
+
+#include "common.h"
+
+/** The struct that is stored in device->data for this type of device. */
+struct temper_type_hid_device_data
+{
+	/** Handle for the HID device. */
+	hid_device *hid_dev;
+	
+	/** Length of data that has been read from the device. */
+	int data_length;
+	
+	/** The data that has been read from the device. */
+	unsigned char data[64];
+	
+	/** The HID device info for this device. */
+	struct temper_type_hid_data *dev_info;
+};
+
+#endif
